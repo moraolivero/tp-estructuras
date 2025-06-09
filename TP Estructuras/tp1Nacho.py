c9468cff2dd0ede92fee4eb5c1af25ef5e0ad85b
Nachosame <isameghini@itba.edu.ar>
2025-06-09 17:42:35 -0300

    Co-authored-by: renacastagnino <renacastagnino@users.noreply.github.com> Co-authored-by: moraolivero <moraolivero@users.noreply.github.com>

--- TP Estructuras/tp1Nacho.py
+++ TP Estructuras/tp1Nacho.py
@@ -1,4 +1,5 @@
 import csv
+
 import math
 from typing import List, Dict, Set, Optional
 
@@ -330,46 +331,3 @@ visitar_hijos(Zarate, [Junin]):
 
 
 
-# Crear nodos
-zarate = Nodo("Zarate", {"Ferroviario", "Automotor", "Maritimo"})
-buenos_aires = Nodo("Buenos Aires", {"Ferroviario", "Automotor", "Maritimo"})
-mar_del_plata = Nodo("Mar del Plata", {"Ferroviario", "Automotor", "Maritimo"})
-
-# Crear conexiones según pdf:
-# Zárate → Buenos Aires: 85 km
-c1 = Conexion(zarate, buenos_aires, "Ferroviario", 85, 100)
-c1b = Conexion(zarate, buenos_aires, "Automotor", 85, 80)
-c1c = Conexion(zarate, buenos_aires, "Maritimo", 85, 40)
-
-# Buenos Aires → Mar del Plata: 384 km
-c2 = Conexion(buenos_aires, mar_del_plata, "Ferroviario", 384, 100)
-c2b = Conexion(buenos_aires, mar_del_plata, "Automotor", 384, 80)
-c2c = Conexion(buenos_aires, mar_del_plata, "Maritimo", 384, 40)
-
-# Agregar conexiones
-zarate.agregar_conexion(c1); zarate.agregar_conexion(c1b); zarate.agregar_conexion(c1c)
-buenos_aires.agregar_conexion(c2); buenos_aires.agregar_conexion(c2b); buenos_aires.agregar_conexion(c2c)
-
-
-vehiculos = [
-    Ferroviario("Ferroviario", velocidad=100, capacidad=150000, costo_fijo=100, costo_x_km=20, costo_x_kg=3),
-    Automotor("Automotor", velocidad=80, capacidad=30000, costo_fijo=30, costo_x_km=5, costo_x_kg=1),
-    Maritimo("Maritimo", velocidad=40, capacidad=100000, costo_fijo=500, costo_x_km=15, costo_x_kg=2),
-]
-
-
-
-planificador = Planificador(vehiculos)
-
-# Solicitud: 70.000 kg desde Zárate hasta Mar del Plata
-carga = 70000
-
-# KPI: tiempo
-it_tiempo = planificador.planificar_itinerario(zarate, mar_del_plata, carga, kpi="tiempo")
-print("**Optimizando TIEMPO**:")
-print(it_tiempo)
-
-# KPI: costo
-it_costo = planificador.planificar_itinerario(zarate, mar_del_plata, carga, kpi="costo")
-print("**Optimizando COSTO**:")
-print(it_costo)
